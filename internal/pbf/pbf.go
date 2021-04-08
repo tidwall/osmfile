2f0e97feb2f39af92dca151d3ed2188fd7348394
tidwall <joshbaker77@gmail.com>
2021-04-08 12:09:35 -0700

    Add MIT License

--- internal/pbf/pbf.go
+++ internal/pbf/pbf.go
@@ -1,4 +1,6 @@
-// Copyright 2018 Joshua J Baker. All rights reserved.
+// Copyright 2021 Joshua J Baker. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 
 package pbf
 
--- reader.go
+++ reader.go
@@ -1,4 +1,6 @@
-// Copyright 2020 Joshua J Baker. All rights reserved.
+// Copyright 2021 Joshua J Baker. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
 
 package osmfile
 
